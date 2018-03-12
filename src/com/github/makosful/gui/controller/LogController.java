d190ea517df63c6e928117aefd448719493b214c
Axl <makosful@gmail.com>
2018-03-12 17:47:40 +0100

    Fixed the undo/redo

--- src/com/github/makosful/gui/controller/LogController.java
+++ src/com/github/makosful/gui/controller/LogController.java
@@ -3,7 +3,6 @@ package com.github.makosful.gui.controller;
 import com.github.makosful.gui.Model;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.Stack;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,7 +15,8 @@ import javafx.scene.layout.BorderPane;
  *
  * @author Axl
  */
-public class LogController implements Initializable {
+public class LogController implements Initializable
+{
 
     private Model model;
 
@@ -38,49 +38,68 @@ public class LogController implements Initializable {
      * @param rb
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb)
+    {
         model = Model.getInstance();
 
         lstLog.setItems(model.getLogList());
     }
 
     @FXML
-    private void handleSend(ActionEvent event) {
-        model.fxmlSend(txtMessage.getText());
-        lstLog.scrollTo(lstLog.getItems().size());
+    private void handleSend(ActionEvent event)
+    {
+        send();
     }
 
     @FXML
-    private void handleDelete(ActionEvent event) {
+    private void handleDelete(ActionEvent event)
+    {
         model.fxmlDelete(lstLog.getSelectionModel().getSelectedItem());
         lstLog.getSelectionModel().select(-1);
     }
 
-    private void handleDeleteAll(ActionEvent event) {
+    private void handleDeleteAll(ActionEvent event)
+    {
         model.fxmlDeleteAll();
     }
 
     @FXML
-    private void handleRedo(ActionEvent event) {
+    private void handleRedo(ActionEvent event)
+    {
         model.redoChange();
-        System.out.println("Redo");
     }
 
     @FXML
-    private void handleUndo(ActionEvent event) {
+    private void handleUndo(ActionEvent event)
+    {
         model.undoChange();
-        System.out.println("Undo");
     }
 
     @FXML
-    private void handleOpen(ActionEvent event) {
+    private void handleOpen(ActionEvent event)
+    {
     }
 
     @FXML
-    private void handleSave(ActionEvent event) {
+    private void handleSave(ActionEvent event)
+    {
     }
 
     @FXML
-    private void handleClose(ActionEvent event) {
+    private void handleClose(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void handleText(ActionEvent event)
+    {
+        send();
+    }
+
+    private void send()
+    {
+        model.fxmlSend(txtMessage.getText());
+        lstLog.scrollTo(lstLog.getItems().size());
+        txtMessage.clear();
     }
 }
