686d45d9f64a71cdac6ad2af258e534d91a5a68e
dankphetamine <asge0907@easv365.dk>
2018-04-10 10:43:32 +0200

    login fxml

---
+++ src/com/github/makosful/gui/controller/CollaboratorsController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.makosful.gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Storm
+ */
+public class CollaboratorsController implements Initializable {
+
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private PasswordField txtPassword;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void clickLogin(ActionEvent event) {
+    }
+
+}
